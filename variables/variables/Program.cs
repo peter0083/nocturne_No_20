bc4fcbf297b70a602ce28e2485bee14598e414b8
ptylin <peter.tingyao@alumni.ubc.ca>
2018-11-15 11:49:56 -0800

    add string concat by user input

--- variables/variables/Program.cs
+++ variables/variables/Program.cs
@@ -21,7 +21,18 @@ namespace variables
             Console.ReadLine();
             */
 
-            Console.WriteLine("Enter username:");
+            Console.WriteLine("Enter user ID and pin...");
+            Console.Write("Type your user ID here: ");
+
+            string myUserID;
+            myUserID = Console.ReadLine();
+
+            Console.Write("Type your user pin here: ");
+
+            string myUserPin;
+            myUserPin = Console.ReadLine();
+
+            Console.WriteLine("User ID " + myUserID + " is now paired with " + myUserPin);
             Console.ReadLine();
         }
     }
