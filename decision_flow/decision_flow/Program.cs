cbc8c68c9ce4f692f86c842e0989e959852cf613
ptylin <peter.tingyao@alumni.ubc.ca>
2018-11-15 12:27:01 -0800

    monty hall simulator

--- decision_flow/decision_flow/Program.cs
+++ decision_flow/decision_flow/Program.cs
@@ -10,6 +10,18 @@ namespace decision_flow
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Initiating Monty Hall Simulator...");
+            Console.Write("Choose door 1, 2, or 3: ");
+
+            string userDoorPick = Console.ReadLine();
+
+            if (userDoorPick == "1")
+            {
+                string winMessage = "You won a new car!";
+                Console.WriteLine(winMessage);
+            }
+
+            Console.ReadLine();
         }
     }
 }
