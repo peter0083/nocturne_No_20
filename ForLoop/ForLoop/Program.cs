0c185231e363b29f8f7906ad33d10a3aeae8aa30
ptylin <peter.tingyao@alumni.ubc.ca>
2018-11-15 15:35:29 -0800

    for loop exercise

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int index = 0; index < 10; index++)
+            {
+                Console.WriteLine(index);
+            }
+            Console.ReadLine();
+        }
+    }
+}
