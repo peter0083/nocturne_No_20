b794c6ff3b96bd9bf54cf03e99dedd08569a44ef
ptylin <peter.tingyao@alumni.ubc.ca>
2018-11-20 20:17:13 -0800

    learning different properties of array

--- learnArray/learnArray/Program.cs
+++ learnArray/learnArray/Program.cs
@@ -10,6 +10,17 @@ namespace learnArray
     {
         static void Main(string[] args)
         {
+            int[] testArray = new int[5];
+
+            testArray[0] = 4;
+            testArray[1] = 8;
+            testArray[2] = 15;
+            testArray[3] = 16;
+            testArray[4] = 23;
+
+            Console.WriteLine(testArray[1]);
+            Console.WriteLine(testArray.Length);
+            Console.ReadLine();
         }
     }
 }
